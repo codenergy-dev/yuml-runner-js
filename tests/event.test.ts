4267f7d2d226edd9abe4a7013801000aa257fd8a
Lucas Lannes <lucaslannesr@gmail.com>
2025-08-22 14:13:11 -0300

    Add test to trigger pipeline from another workflow event

---
+++ tests/event.test.ts
@@ -0,0 +1,22 @@
+import { PipelineState } from '../src/pipeline'
+import { Workflows } from '../src/workflows'
+import { readWorkflowJson } from './utils/read-workflow-json'
+
+describe('event', () => {
+  it('trigger pipeline from another workflow event', async () => {
+    const workflows = Workflows.fromJson([
+      ...readWorkflowJson('a-b.json'),
+      ...readWorkflowJson('event.json'),
+    ])
+    workflows.bindModules({
+      'a-b': () => import('./pipelines/a-b'),
+      'event': () => import('./pipelines/event')
+    })
+
+    const onPipelineEvent = jest.fn()
+    workflows.events.on('event', onPipelineEvent)
+
+    const pipelines = await workflows.run('a-b', 'a')
+    expect(onPipelineEvent).toHaveBeenCalledTimes(1)
+  })
+})
